262b7e49376e1caec59f5c02724c4d31aa458cf3
Colby Melvin <colby.g.melvin@gmail.com>
2026-02-03 21:30:16 -0500

    first commit, moving all code from other repos

---
+++ monitoring/uninstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eou pipefail
+
+# Stop the services now
+systemctl --user stop grafana
+systemctl --user stop node-exporter
+systemctl --user stop prometheus
+
+# Remove the container files from ~/.config/containers/systemd
+rm -rf ~/.config/containers/systemd/homelab-monitoring/
+
+# Reload unit files and rebuild dependency trees 
+systemctl --user daemon-reload
